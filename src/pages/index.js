89bd87263e0424dc9c99fd4b814f92e50b94ae31
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-06-24 13:57:57 +0100

    add right

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,14 @@ export default ({ data }) => {
   	</div>
 
 		<div class="previews">
-		  <div class="preview main">
+		  <div class="preview">
+		    <h3 class="sub-section-heading">Here is a title</h3>
+		    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		  </div>  
+		</div>
+
+		<div class="previews">
+		  <div class="preview right">
 		    <h3 class="sub-section-heading">Here is a title</h3>
 		    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 		  </div>  
