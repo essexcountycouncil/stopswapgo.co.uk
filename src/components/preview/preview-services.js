4160f6300bf05296eddfcb54cd7652cabae619cc
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-07-07 19:19:07 +0100

    preview tweak

--- src/components/preview/preview-services.js
+++ src/components/preview/preview-services.js
@@ -3,11 +3,11 @@ import cyclist from "./Chelmsford-station-cyclist.jpg"
 
 export default () =>
 <>
-	<div class="previews">
+	<div class="previews block">
 	  <div class="preview">
 	    <img src={cyclist} alt="" width="" height="" />
 	  </div>
-	  <div class="preview block">
+	  <div class="preview">
 	    <h3 class="sub-section-heading">Join our community</h3>
 	    <p>Join our community @stopswapgo for the latest events, news and journey planning info. We’ll also be sharing your stories and experiences of walking and cycling. And sign up to our newsletter (below) for details on how you can join our 60 Day Challenge and win prizes.</p>
 	    <ul class="social">
