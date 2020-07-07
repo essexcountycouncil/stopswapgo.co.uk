d36bde88fd6da4a3d39a434794da08c81b9c788b
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-07-07 18:55:41 +0100

    home image, signup, brand typog

---
+++ src/pages/60-day-challenge.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../layout/layout"
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>60 Day Challenge</h1>
+  </Layout>
+)
+
+export default NotFoundPage
