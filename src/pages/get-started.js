219824e6698e1ab0313f49a3acc989a4a4e9d1c5
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-07-07 13:37:47 +0100

    add basic get started page

---
+++ src/pages/get-started.js
@@ -0,0 +1,79 @@
+import React from "react"
+
+import Layout from "../layout/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>Get started</h1>
+		<p>Here you&rsquo;ll find a treasure trove of essential advice, top tips and guidance on how to get the most out of walking and cycling.</p>
+		<p>Whether you&rsquo;re looking for a more scenic cycling route, or an excuse to get out, there&rsquo;s something for everyone here. We&rsquo;ve pulled together all the maps, apps and travel hacks you need to make it easy and enjoyable to:</p>
+		<ul>
+			<li><strong>Stop</strong> making car journeys to work, the shops or school</li> 
+			<li><strong>Swap</strong> to more active and sustainable travel options</li>
+			<li><strong>GO!</strong> and make the most of your day &ndash; and feel great!</li>  
+		</ul>
+
+		<p>--</p>
+		
+		<h2>Get started with walking and cycling in Essex</h2>
+		<h3>Latest news and advice for getting around in Essex</h3>
+		<p>Visit Essex Highways for the latest news, tips and guidance on getting around in Essex. A great place to start.</p>
+
+		<h3>Be Safer, Greener, Healthier</h3>
+		<p>Essex County Council and Essex Highways are working hard to make our communities safer, greener and healthier. From additional cycle lanes and pedestrianised zones to reduced speed limits and parking, they&rsquo;ve introduced loads of measures to help you social distance. See what&rsquo;s coming to your town on the Essex Highways website.</p>
+
+		<h3>Essex cycle maps and route guides</h3>
+		<p>If you&rsquo;re taking a car trip of under 5km, it&rsquo;s easy to switch to cycling or walking.</p>
+		<p>Check out these free maps and route guides from Essex Highways, wherever you are in Essex.</p>
+
+		<h3>Discover great places to visit in Essex</h3>
+		<p>Check out picturesque walking routes and fun cycling routes on the Visit Essex website, as well as up-to-date tourist information on great places to visit near you.</p>
+
+		<h3>Explore hidden gems on your doorstep</h3>
+		<p>Another great website to inspire you to explore secret spots in Essex and get active with the whole family. Find out more on the Explore Essex website.</p>
+
+		<h3>Get two-wheeled tips and inspiration from people in Essex</h3>
+		<p>Love to Ride Essex is a brilliant community for two-wheeled tips and inspirational cycling stories.</p>
+
+		<h3>Safety information for all road users</h3>
+		<p>Find general safety information for all road users on the Safer Essex Roads Partnership website.</p>
+
+		<h3>Free adult and family cycle training</h3>
+		<p>Need a confidence boost to get back in the saddle or take to two wheels for the first time? Free cycle training from the Safer Essex Roads Partnership will have you moving in no time.</p>
+
+		<h3>Chelmsford is changing!</h3>
+		<p>The Chelmsford City Growth Package is all about improving walking and cycling infrastructure in the city. Learn more about upcoming features and when you can start using them on the Essex Highways website.</p>
+
+		<h3>Cycling and walking guidance for South Essex</h3>
+		<p>Just like us, Forward Motion wants people to switch from driving to cycling or walking instead. Their website is full of travel tips and guidance for doing just that.</p>
+
+		<h2>More cycling and walking resources</h2>
+		<h3>Try mindful movement</h3>
+		<p>How do you get better at walking? By combining it with yoga and Pilates! Simply Stride runs online classes to help you get moving and be more mindful.</p>
+
+		<h3>Share walks with your friends</h3>
+		<p>Want to discover great walks near you and create your own to share with friends? Then Go Jauntly is the app for you.</p>
+
+		<h3>Want free cake for cycling?</h3>
+		<p>Join the Cake Escape, ride to its scenic cafes, collect stamps, and that free cake is all yours. Yes, really. Sign us up!</p>
+
+		<h3>Advice on walking to school and work</h3>
+		<p>Living Streets is the national charity for everyday walking. The website offers lots of advice to help you walk to school and work.</p>
+
+		<h3>Tips for getting started with walking and cycling</h3>
+		<p>Sustrans is a UK charity making it easier for everyone to walk and cycle. They create new routes all the time, and offer tips on everything from buying a bike to planning your ride. Be sure to check out their searchable network of safe, sustainable cycle routes.</p>
+
+		<h3>Changing lives through cycling</h3>
+		<p>Cycling UK is a charity which aims to change lives and communities through cycling. Sounds good to us. The website&rsquo;s full of advice, events and routes for all levels of experience. Let&rsquo;s get rolling.</p>
+
+		<h2>Guidance for businesses and employees</h2>
+		<h3>Travel plans for Essex</h3>
+		<p>Essex County Council has big plans for the future of business and personal travel. Check out the Sustainable Modes of Travel Plan and get advice to create a travel plan for your business on the Essex County Council website.</p>
+
+		<h3>Join the Colchester Travel Plan Club</h3>
+		<p>The Colchester Travel Plan Club is a network of businesses and organisations committed to promoting active and sustainable travel for employees. Learn more and join up for free on the Colchester Travel Plan Club website.</p>
+  </Layout>
+)
+
+export default NotFoundPage
