bf13a145b71d8a9aea5e79246455e25f19836b37
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-07-07 20:39:32 +0100

    add 60 day copy

--- src/pages/60-day-challenge.js
+++ src/pages/60-day-challenge.js
@@ -1,10 +1,19 @@
 import React from "react"
 import Layout from "../layout/layout"
 
-const NotFoundPage = () => (
+const Challenge = () => (
   <Layout>
     <h1>60 Day Challenge</h1>
+    <h2>Win prizes for walking and cycling with our 60 Day Challenge.</h2>
+
+    <p>That’s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic prizes.</p>
+		
+		<p>Our inaugural 60 Day Challenge season starts on 1 August 2020. We’ll be giving away loads of great prizes including gift vouchers, walking and cycling accessories – and even the chance to win yourself a bike.</p>
+		
+		<p>All you have to do to be in with a chance to win is sign up to the challenge, download our challenge app and share your daily walking and cycling trips with us. Hint: the more you walk and cycle over the 60 days, the more chances you’ll have to win.   It couldn’t be easier to Stop. Swap. WIN! Early bird registrations open on Monday 13 July and we’ll be giving the first 500 lucky VIPs on our waiting list an extra prize once they start their 60 day challenge. It’s our way of saying thank you for supporting Stop. Swap. GO! </p>
+		
+		<p>If you’d like to be the first to hear when our waitlist goes live, sign up to our newsletter now and we’ll ping you an update when we’re ready to GO! </p>
   </Layout>
 )
 
-export default NotFoundPage
+export default Challenge
