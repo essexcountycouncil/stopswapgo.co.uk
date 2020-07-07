a8dde008642d4d21b960dcce4ab599785ba7ebcb
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-07-07 19:06:21 +0100

    first pass update

--- src/components/preview/preview-services.js
+++ src/components/preview/preview-services.js
@@ -7,7 +7,7 @@ export default () =>
 	  <div class="preview">
 	    <img src={cyclist} alt="" width="" height="" />
 	  </div>
-	  <div class="preview">
+	  <div class="preview block">
 	    <h3 class="sub-section-heading">Join our community</h3>
 	    <p>Join our community @stopswapgo for the latest events, news and journey planning info. We’ll also be sharing your stories and experiences of walking and cycling. And sign up to our newsletter (below) for details on how you can join our 60 Day Challenge and win prizes.</p>
 	    <ul class="social">
