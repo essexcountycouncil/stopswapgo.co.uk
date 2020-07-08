bf7baa031e78aa7ae961b1113eebd36a9fbf8ee6
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-07-08 11:36:39 +0100

    mobile support

--- src/components/preview/preview-services.js
+++ src/components/preview/preview-services.js
@@ -10,7 +10,7 @@ export default () =>
 	  </div>
 	  <div class="preview">
 	    <h3 class="sub-section-heading">Join our community</h3>
-	    <p>Join our community @stopswapgo for the latest events, news and journey planning info. We’ll also be sharing your stories and experiences of walking and cycling. Sign up to our newsletter (below) for regular Stop. Swap. GO! community, walking and cycling updates and details of how you can join our 60 Day Challenge to win prizes.</p>
+	    <p>Join our community @stopswapgo for the latest events, news and journey planning info. We’ll also be sharing your stories and experiences of walking and cycling. Sign up to our newsletter (below) for regular Stop. Swap. <i>GO!</i> community, walking and cycling updates and details of how you can join our 60 Day Challenge to win prizes.</p>
 	    <ul class="social">
 	      <li>
 	        <a href="https://www.facebook.com/stopswapgo" class="facebook"><span class="fab fa-facebook-f"></span><span>Facebook</span></a></li><li>
--- src/pages/60-day-challenge.js
+++ src/pages/60-day-challenge.js
@@ -6,13 +6,22 @@ const Challenge = () => (
     <h1>60 Day Challenge</h1>
     <h2>Win prizes for walking and cycling with our 60 Day Challenge.</h2>
 
-    <p>That’s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic prizes.</p>
+    <p>That’s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic
+prizes.</p>
 		
-		<p>Our 60 Day Challenge season starts on 1 August 2020. We’ll be giving away loads of great prizes including gift vouchers, walking and cycling accessories – and even the chance to win yourself a bike.</p>
+		<p>Our 60 Day Challenge starts from 1 August 2020. We’ll be giving away loads of great prizes including
+gift vouchers, walking and cycling accessories – and even the chance to win yourself a bike.</p>
 		
-		<p>All you have to do to be in with a chance to win is sign up to the challenge, download our challenge app and share your daily walking and cycling trips with us. Hint: the more you walk and cycle over the 60 days, the more chances you’ll have to win.   It couldn’t be easier to Stop. Swap. WIN! Early bird registrations open on Monday 13 July and we’ll be giving the first 500 lucky VIPs on our waiting list an extra prize once they start their 60 day challenge. It’s our way of saying thank you for supporting Stop. Swap. GO! </p>
+		<p>All you have to do to be in with a chance to win is sign up to the 60 Day Challenge, download our
+app and share your daily walking and cycling trips with us. Hint: the more you walk and cycle over
+the 60 days, the more chances you’ll have to win.</p>
+
+		<p>It couldn’t be easier to Stop. Swap. WIN! Early bird registrations open on Monday 13 July and we’ll
+		be giving the first 500 lucky VIPs on our waiting list an extra prize once they start their 60 Day
+		Challenge. It’s our way of saying thank you for supporting Stop. Swap. <i>GO!</i></p>
 		
-		<p>If you’d like to be the first to hear when our waitlist goes live, sign up to our newsletter now and we’ll ping you an update when we’re ready to GO! </p>
+		<p>If you’d like to be the first to hear when our waitlist goes live, sign up to our newsletter now and
+we’ll ping you an update when we’re ready to <i>GO!</i></p>
   </Layout>
 )
 
