757378a9c2872cc98933f314133619397af56dd3
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-07-10 13:37:34 +0100

    change twitter handle

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -16,7 +16,7 @@ export default () =>
           </ul>
           <ul className="social">
             <li>
-              <a href="https://www.twitter.com/stopswapgo"><span className="fab fa-twitter"></span><span>Twitter</span></a></li><li>
+              <a href="https://www.twitter.com/stopswapgoessex"><span className="fab fa-twitter"></span><span>Twitter</span></a></li><li>
               <a href="https://www.facebook.com/stopswapgo"><span className="fab fa-facebook-f"></span><span>Facebook</span></a></li><li>
               <a href="https://instagram.com/stopswapgo"><span className="fab fa-instagram"></span><span>Instagram</span></a></li>
           </ul>
