c0e93f5111c327e33a86bb0b08aa05ac92e51eb2
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-07-10 07:48:58 +0100

    add faq page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -14,12 +14,12 @@ function Header() {
             </a>
           </div>
           <ul className="nav-links">
-            <li>
-              <a href="/">Home</a></li><li>
-              <a href="/about-us">About us</a></li><li>
-              <a href="/get-started">Get started</a></li><li>
-              <a href="/60-day-challenge">60 Day Challenge</a></li>
-          </ul>        
+            <li><a href="/">Home</a></li>
+            <li><a href="/about-us">About us</a></li>
+            <li><a href="/get-started">Get started</a></li>
+            <li><a href="/60-day-challenge">60 Day Challenge</a></li>
+            <li><a href="/faq">FAQ</a></li>
+          </ul>
         </div>
       </div>
     </header>
--- src/pages/60-day-challenge.js
+++ src/pages/60-day-challenge.js
@@ -6,22 +6,22 @@ const Challenge = () => (
     <h1>60 Day Challenge</h1>
     <h2>Win prizes for walking and cycling with our 60 Day Challenge.</h2>
 
-    <p>That’s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic
+    <p>That&rsquo;s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic
 prizes.</p>
 		
-		<p>Our 60 Day Challenge starts from 1 August 2020. We’ll be giving away loads of great prizes including
+		<p>Our 60 Day Challenge starts from 1 August 2020. We&rsquo;ll be giving away loads of great prizes including
 gift vouchers, walking and cycling accessories – and even the chance to win yourself a bike.</p>
 		
 		<p>All you have to do to be in with a chance to win is sign up to the 60 Day Challenge, download our
 app and share your daily walking and cycling trips with us. Hint: the more you walk and cycle over
-the 60 days, the more chances you’ll have to win.</p>
+the 60 days, the more chances you&rsquo;ll have to win.</p>
 
-		<p>It couldn’t be easier to Stop. Swap. WIN! Early bird registrations open on Monday 13 July and we’ll
+		<p>It couldn&rsquo;t be easier to Stop. Swap. WIN! Early bird registrations open on Monday 13 July and we&rsquo;ll
 		be giving the first 500 lucky VIPs on our waiting list an extra prize once they start their 60 Day
-		Challenge. It’s our way of saying thank you for supporting Stop. Swap. <i>GO!</i></p>
+		Challenge. It&rsquo;s our way of saying thank you for supporting Stop. Swap. <i>GO!</i></p>
 		
-		<p>If you’d like to be the first to hear when our waitlist goes live, sign up to our newsletter now and
-we’ll ping you an update when we’re ready to <i>GO!</i></p>
+		<p>If you&rsquo;d like to be the first to hear when our waitlist goes live, sign up to our newsletter now and
+we&rsquo;ll ping you an update when we&rsquo;re ready to <i>GO!</i></p>
   </Layout>
 )
 
