9c79a5cbf3a3a9ee78e98ef4bf4ab66f7191322e
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-07-10 14:22:19 +0100

    add fixes

--- src/components/govdelivery-form/govdelivery-form.js
+++ src/components/govdelivery-form/govdelivery-form.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default ({style, id, formId, title}) => 
     <form id={id} action={"https://public.govdelivery.com/accounts/UKESSEX/signup/"+formId} className="form">
     <div className="group">
-      <div className="signup-button" id="submitYourEmailAddress">
+      <div className="signup-button" id={"submitYourEmailAddress-"+formId}>
 <button type="submit" className={style + " button button-large"} value="Subscribe">{title}</button>
       </div>
     </div>
--- src/pages/60-day-challenge.js
+++ src/pages/60-day-challenge.js
@@ -7,12 +7,14 @@ const Challenge = () => (
 		<h1>60 Day Challenge</h1>
 		<h2>Win prizes for walking and cycling with our 60 Day Challenge.</h2>
 
-		<div className="notice">
-			<p >Early bird VIP registrations for our 60 Day Challenge (launching 1 August 2020) are now open.
-Register using the form and get ready to Stop. Swap. <i>GO!</i></p>
-		</div>
 
 		<div className="text-block">
+
+
+			<div className="notice notice-middle">
+				<h3>Early bird VIP registrations for our 60 Day Challenge (launching 1 August 2020) are now open.</h3>
+				<p>Register using the form and get ready to Stop. Swap. <i>GO!</i></p>
+			</div>
 			<p>That&rsquo;s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic
 		prizes.</p>
 
@@ -34,15 +36,15 @@ prize as a thank you for supporting Stop. Swap. <i>GO!</i></p>
 
 			<p>And that's not all.</p>
 
+			<div className="notice-middle">
+				<h3>One lucky VIP will win a bike plus cycling accessories to the value of £1,000!</h3>
+				<p>So, what are you waiting for? Fill in the form below and get ready to Stop. Swap. <i>GO!</i></p>
+			</div>
 
 		</div>
 		<div className="text-align-center">
-			<p className="bold">**One lucky VIP will win a bike plus cycling accessories to the value of £1,000!**</p>
-
-			<p>So, what are you waiting for? Fill in the form below and get ready to Stop. Swap. <i>GO!</i></p>
-			<br/>
 			<GovDeliveryForm id={"mid-data-signup-id"} formId="17087" style="button-dark" title="SUBMIT!" />
-			
+
 		</div>
 		<h2>FAQs</h2>
 		<p>You can find the 60 Day Challenge FAQs page <a href="/faq">here</a></p>
