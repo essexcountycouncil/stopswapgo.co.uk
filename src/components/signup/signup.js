0be4640695df5a171170bc4fe8be4f339ea8119c
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-07-10 13:09:47 +0100

    add more stage 2 fixes

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,5 +1,5 @@
 import React from "react"
-import NewsletterForm from "../newsletter-form/newsletter-form"
+import GovDeliveryForm from "../govdelivery-form/govdelivery-form"
 
 export default () =>
 <section className="signup-background" aria-label="Subscribe to email" id="challenge">
@@ -7,7 +7,8 @@ export default () =>
     <div className="mantra">
       <h2 className="strapline"><span>Keep on moving</span></h2>
       <p>Sign up to our newsletter for the latest walking and cycling news, tips, and exclusive offers to keep moving. You’ll also be the first to hear about our 60 Day Challenge where you can win prizes for cycling and walking. Just pop in your name, email and hit <i>GO!</i> We’ll do the rest.</p>
+    
+      <GovDeliveryForm id={"data-signup-id"} style="" formId="16846"/>
     </div>
-    <NewsletterForm id={"data-signup-id"} style=""/>
   </div>
 </section>
