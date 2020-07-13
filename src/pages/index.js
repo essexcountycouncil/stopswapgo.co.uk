7d0c60292e453d9a098f449390619e4c72238d84
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-07-13 12:31:05 +0100

    make small fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default ({ data }) => {
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous" />
     </Helmet>
     <div className="mantra" id="get-ready">
-    	<h2 className="strapline">Get ready to Stop.Swap.<i>GO!</i></h2>
+    	<h2 className="strapline">Get ready to Stop. Swap. <i>GO!</i></h2>
       <br/>
     	<p>Hello! We’re making getting around Essex easier – and more rewarding – than ever.</p>
       <p>More and more of us are <span>swapping from car to bus, bike or our own two feet</span>. And whether you’re off to the shops, visiting friends or family, or getting to and from work, we’re here to help keep you moving.</p>
