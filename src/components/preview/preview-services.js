76bca0f434808604eaeccff3623feb8d4939891a
Riccardo Noviello <riccardo.noviello@gmail.com>
2020-07-15 10:30:38 +0100

    add boxes

--- src/components/preview/preview-services.js
+++ src/components/preview/preview-services.js
@@ -6,12 +6,13 @@ export default () =>
 	<div>
 
 		<div className="previews">
-			<div className="preview">
-				<img src={cyclistPicture} className="preview-img" alt="Woman with a bicycle outside Chelmsform Railway Station" title="Woman with a bicycle outside Chelmsform Railway Station" />
-			</div>
+
 			<div className="preview">
 				<h3 className="sub-section-heading strapline strapline-highlighted"><span>Stop.Swap.</span><i>WIN!</i></h3>
-				<div className="preview-grey-border"><p>Want to win prizes for walking and cycling? Of course you do. Head over to our 60 Day Challenge
+				<div className="preview-grey-border">
+				<img src={cyclistPicture} alt="Woman with a bicycle outside Chelmsform Railway Station" title="Woman with a bicycle outside Chelmsform Railway Station" />
+			
+					<p>Want to win prizes for walking and cycling? Of course you do. Head over to our 60 Day Challenge
 page to learn more and register for exclusive VIP access and early bird rewards.</p>
 					<LargeButton title="WIN!" url="/60-day-challenge" /></div>
 			</div>
@@ -20,7 +21,9 @@ page to learn more and register for exclusive VIP access and early bird rewards.
 		<div className="previews">
 			<div className="preview">
 				<h3 className="sub-section-heading strapline strapline-highlighted"><span>Stop.Swap.</span><i>SHARE!</i></h3>
-				<div className="preview-grey-border"><p>Follow us and share your active travel stories, tips and tricks. #StopSwapGo</p>
+				<div className="preview-grey-border">
+				<img src={familyPicture} alt="Mother, father and young child walking in a park" title="Mother, father and young child walking in a park" />
+					<p>Follow us and share your active travel stories, tips and tricks. #StopSwapGo</p>
 					<ul className="social">
 						<li>
 							<a href="https://www.twitter.com/stopswapgoessex" className="twitter"><span className="fab fa-twitter"></span><span>Twitter</span></a></li><li>
@@ -28,8 +31,5 @@ page to learn more and register for exclusive VIP access and early bird rewards.
 							<a href="https://instagram.com/stopswapgo" className="instagram"><span className="fab fa-instagram"></span><span>Instagram</span></a></li>
 					</ul>
 				</div></div>
-			<div className="preview">
-				<img src={familyPicture} className="preview-img" alt="Mother, father and young child walking in a park" title="Mother, father and young child walking in a park" />
-			</div>
 		</div>
 	</div>
