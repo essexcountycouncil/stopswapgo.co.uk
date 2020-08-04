5a7cd100b0e591c2a5764987d9cb4139706a00fc
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-04 17:12:25 +0100

    remove odd characters

--- src/pages/getting-to-school/prepare.js
+++ src/pages/getting-to-school/prepare.js
@@ -24,30 +24,30 @@ const Prepare = () => (
 		<h2>Swap &ndash; to cycling, walking, or scooting</h2>
 		<p>It really is super easy to get out of the car and onto better ways of getting to school. Here&rsquo;s some advice for overcoming those first hurdles to swapping.</p>
 		
-		<h2> &lsquo;We live too far to walk, cycle, or scoot to school&rsquo;&#8232;</h2>
+		<h2> &lsquo;We live too far to walk, cycle, or scoot to school&rsquo;</h2>
 		<p>That&rsquo;s okay! Your best bet would be to drive to one of the drop-off points that you can find on our maps. Our maps will eventually show <Link to="/getting-to-school/routes">suggested drop-off and pick-up points for every secondary school in Essex</Link>. We&rsquo;ve tried and tested them and they&rsquo;re the best bet to help you avoid the traffic and get your children to school on time.</p>
 		
-		<h2> &lsquo;My children always get the bus&rsquo;&#8232;</h2>
+		<h2> &lsquo;My children always get the bus&rsquo;</h2>
 		<p>If you live close enough then a swap to cycling or walking in is your best bet. The <Link to="https://www.essex.gov.uk/school-transport/using-school-transport-guidance-for-parents">latest government guidance on school transport</Link> says that bus operators need to limit the number of people on board, to maintain social distancing, and <Link to="https://www.gov.uk/government/publications/face-coverings-when-to-wear-one-and-how-to-make-your-own/face-coverings-when-to-wear-one-and-how-to-make-your-own">most over the age of 11 will need to wear masks</Link>.  We all know how packed the buses can get during a school run, so if you live close to school your child could be waiting a long time to get a space on the bus. The same goes coming home from school. Check with local bus operators for more information.</p>
 		
-		<h2> &lsquo;I just drive my children to school&rsquo;&#8232;</h2>
-		<p>There&rsquo;s no better way to save money and get your children healthier on the school run than scooting, cycling or walking to school! One upside of the current crisis is that roads have been quieter, and we&rsquo;ve had more time to try new ways of getting about. Check out our <Link to="https://www.instagram.com/stopswapgo/">Instagram</Link>, <Link to="https://www.facebook.com/stopswapgo">Facebook</Link>, and <Link to="https://twitter.com/stopswapgoessex">Twitter</Link> to see how people have been getting around Essex.&#8232;Give your child the chance to breathe cleaner air, learn how to get around without you, and spend time with their friends. All whilst you avoid the traffic which could make them late. &#8232;</p>
+		<h2> &lsquo;I just drive my children to school&rsquo;</h2>
+		<p>There&rsquo;s no better way to save money and get your children healthier on the school run than scooting, cycling or walking to school! One upside of the current crisis is that roads have been quieter, and we&rsquo;ve had more time to try new ways of getting about. Check out our <Link to="https://www.instagram.com/stopswapgo/">Instagram</Link>, <Link to="https://www.facebook.com/stopswapgo">Facebook</Link>, and <Link to="https://twitter.com/stopswapgoessex">Twitter</Link> to see how people have been getting around Essex.Give your child the chance to breathe cleaner air, learn how to get around without you, and spend time with their friends. All whilst you avoid the traffic which could make them late. </p>
 		
-		<h2> &lsquo;It isn&rsquo;t safe for my child to walk, cycle or scoot to school&rsquo;&#8232;</h2>
+		<h2> &lsquo;It isn&rsquo;t safe for my child to walk, cycle or scoot to school&rsquo;</h2>
 		<p>There are plenty of things you can do to make your child safer on the roads. Safer Essex Roads Partnership offers <Link to="https://saferessexroads.org/road-users/cycling/">road and bike safety</Link> lessons to give your child the skills they need to handle their trip to school. They&rsquo;re great for getting children used to the roads in a safe way. You could even spend a bit of time taking the route with your family over summer, so everyone can feel comfortable and safe.</p>
 
-		<h2>GO! &ndash; what to do from now until school starts&#8232;</h2>
+		<h2>GO! &ndash; what to do from now until school starts</h2>
 		<ol class="bold">
-			<li>Plan your route&#8232;
-				<p>Use our maps that are specific to your route, or think about how to get from your front door to school. Google Maps is another great place to start. You&rsquo;re looking for things like quiet roads, good crossing points, and well-lit areas.&#8232;</p>
+			<li>Plan your route
+				<p>Use our maps that are specific to your route, or think about how to get from your front door to school. Google Maps is another great place to start. You&rsquo;re looking for things like quiet roads, good crossing points, and well-lit areas.</p>
 			</li>
-			<li>Try out your route&#8232;
-				<p>Experiment with routes whilst it isn&rsquo;t busy. You should look out for things like where road crossings are, street lighting (unfortunately it won&rsquo;t be summer forever!), and where the entrances to your school are. This&rsquo;ll help you make sure your plan works or you can change it if you need. You can also work out that your child will get to school on time.&#8232;If you&rsquo;re planning to drive to a drop-off point, it&rsquo;d be good to drive there to see how easy it is to pull in and get the bike or scooter out of the car. &#8232;</p>
+			<li>Try out your route
+				<p>Experiment with routes whilst it isn&rsquo;t busy. You should look out for things like where road crossings are, street lighting (unfortunately it won&rsquo;t be summer forever!), and where the entrances to your school are. This&rsquo;ll help you make sure your plan works or you can change it if you need. You can also work out that your child will get to school on time.If you&rsquo;re planning to drive to a drop-off point, it&rsquo;d be good to drive there to see how easy it is to pull in and get the bike or scooter out of the car. </p>
 			</li>
-			<li>Get your kit together&#8232;
-				<p>If you&rsquo;re cycling to school, take your bike to a local bike shop to make sure it&rsquo;s in working order The government recently launched a cycling scheme which means you can <Link to="https://www.gov.uk/guidance/fix-your-bike-voucher-scheme-apply-for-a-voucher">get &pound;50 off the price of a repair now</Link>. Grab yourself a <Link to="https://www.cyclingweekly.com/news/product-news/7-of-the-best-bike-locks-5221">good bike lock</Link>, front and rear lights, a helmet, and a bell. All these will keep your child and their bike safe. Don&rsquo;t forget a waterproof and a drink to keep them dry and hydrated.&#8232;If you&rsquo;re walking in, that&rsquo;s a little easier. A nice comfy pair of shoes and a rucksack, along with the waterproof and drink again.&#8232;</p>
+			<li>Get your kit together
+				<p>If you&rsquo;re cycling to school, take your bike to a local bike shop to make sure it&rsquo;s in working order The government recently launched a cycling scheme which means you can <Link to="https://www.gov.uk/guidance/fix-your-bike-voucher-scheme-apply-for-a-voucher">get &pound;50 off the price of a repair now</Link>. Grab yourself a <Link to="https://www.cyclingweekly.com/news/product-news/7-of-the-best-bike-locks-5221">good bike lock</Link>, front and rear lights, a helmet, and a bell. All these will keep your child and their bike safe. Don&rsquo;t forget a waterproof and a drink to keep them dry and hydrated.If you&rsquo;re walking in, that&rsquo;s a little easier. A nice comfy pair of shoes and a rucksack, along with the waterproof and drink again.</p>
 			</li>
-			<li>Get ready for the first day back&#8232;
+			<li>Get ready for the first day back
 				<p>Pack bags, set alarms, go back over the route, pump them tyres, see if they&rsquo;re meeting up with people (socially distanced of course!) en route. Doing everything to make day one the easiest it can be means you&rsquo;re more likely to keep the swap going and make a difference to climate change and your child&rsquo;s wellbeing.</p>
 			</li>
 		</ol>
