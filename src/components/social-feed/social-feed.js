cfb25e801c63737245ecc3521d4571d3ad28c9e3
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-04 13:50:03 +0100

    new image, social update and nav addition

--- src/components/social-feed/social-feed.js
+++ src/components/social-feed/social-feed.js
@@ -2,8 +2,16 @@ import React from "react"
 export default () =>
 	<div>
 		<div className="previews social-feed">
-			<h2 className="preview strapline">Check out our social media posts</h2>
 			<div className="preview">
+				<h2 className="strapline">Stop.Swap.<i>SHARE!</i></h2>
+				<p>Join our community on social media and share your active travel stories, tips and tricks. We’ll also be posting live updates on our 60 Day Challenge – including details of our bonus challenges to win more prizes! Keep your eyes peeled, everyone.</p>
+				<p>#StopSwapGo #StopSwapWin</p>
+				<ul className="social">
+					<li>
+						<a href="https://www.twitter.com/stopswapgoessex" className="twitter"><span className="fab fa-twitter"></span><span>Twitter</span></a></li><li>
+						<a href="https://www.facebook.com/stopswapgo" className="facebook"><span className="fab fa-facebook-f"></span><span>Facebook</span></a></li><li>
+						<a href="https://instagram.com/stopswapgo" className="instagram"><span className="fab fa-instagram"></span><span>Instagram</span></a></li>
+				</ul>			
 				<a className="twitter-timeline" data-height="960" href="https://twitter.com/stopswapgoessex?ref_src=twsrc%5Etfw">Tweets by stopswapgoessex</a>
 			</div>
 		</div>
