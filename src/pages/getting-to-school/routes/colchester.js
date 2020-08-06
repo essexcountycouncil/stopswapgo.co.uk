cda39f914ae547656f2b01b05f98b5381ca2a6c2
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-06 09:51:50 +0100

    add pages

---
+++ src/pages/getting-to-school/routes/colchester.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from 'gatsby'
+import Layout from "../../../layout/layout-h2s"
+
+const Colchester = () => (
+	<Layout>
+		<h1>Schools in Colchester</h1>
+
+		<h2>We’re working on your map!</h2>
+		<p>Your route map isn’t ready yet, but your school is on the list. Check back here or follow us on Instagram, Facebook, or Twitter as we’ll launch new routes in for schools over the coming weeks and months.</p>
+		<p>Select your school from the list below to see the available map options:</p>
+		<ul>
+			<li>Colchester County High School</li>
+			<li>Colchester High School</li>
+			<li>Colchester Institute</li>
+			<li>Colchester Royal Grammar</li>
+			<li>Colchester Sixth Form College</li>
+			<li>Philip Morant</li>
+			<li>St Benedicts</li>
+			<li>St Helena</li>
+		</ul>
+
+	</Layout>
+)
+
+export default Colchester
