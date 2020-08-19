436da692165fd08f75b886638093e6a6874aebee
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-19 16:29:54 +0100

    udat url and copy

---
+++ src/pages/getting-to-school/transport.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from 'gatsby'
+import Layout from "../../layout/layout-h2s-with-newsletter-no-banner"
+
+const Transport = () => (
+	<Layout>
+	<h1>Using a dedicated taxi, minibus or bus for education transport</h1>
+	<p>A &lsquo;dedicated service&rsquo; is one that&rsquo;s not open to the public and just takes children to school. In order to keep everyone safe, there will be some changes you need to be aware of</p>
+	<p>We are asking all children who can to wear a <a href="https://www.gov.uk/government/publications/how-to-wear-and-make-a-cloth-face-covering">face covering</a>. Some children are&nbsp;<a href="https://www.gov.uk/guidance/coronavirus-covid-19-safer-travel-guidance-for-passengers#exemptions-face-coverings">exempt from wearing face coverings</a>.</p>
+	<p>Wearing a face covering helps keep everyone safe. It takes a bit of getting used to, but it&rsquo;s a simple measure most children can take. A face covering doesn&rsquo;t have to be a single use mask. It can be something you already have at home, like a scarf.&nbsp;<a href="https://www.gov.uk/government/publications/face-coverings-when-to-wear-one-and-how-to-make-your-own/face-coverings-when-to-wear-one-and-how-to-make-your-own#making-your-own-face-covering">Why not try making your own?</a></p>
+	<p>Please make sure your child has a face covering and knows how to use it. This video shows you how to wear a face covering.</p>
+	<p>&lt;insert video&gt;</p>
+	<p>Other changes you need to be aware of include:</p>
+	<ul>
+		<li>changes to how children get on the bus and where they can sit &ndash; social distancing is not required on dedicated school services so children might be asked to sit with their year group for example</li>
+		<li>changes to pick up and drop off times</li>
+		<li>enhanced cleaning</li>
+	</ul>
+	<p>These measures will vary by school and journey. So make sure your child listens to the transport operator taking them to and from school, and the school teachers.</p>
+	<p>You&rsquo;ll get a letter by the second week of August that will have more information and the contact details of the operator running your service. Alternatively, you can&nbsp;<a href="https://www.stopswapgo.co.uk/getting-to-school/education-transport-operators">contact your transport operator</a>.</p>
+	<p>Please be assured, the transport operators running these services will be following the latest government guidelines.</p>
+	</Layout>
+	)
+
+export default Transport
