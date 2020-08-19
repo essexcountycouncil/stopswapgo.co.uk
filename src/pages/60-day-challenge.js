d7b581c7605dccf90ecf6a63d72765e777453adc
userman123 <dominic.freeman123@gmail.com>
2020-08-19 14:52:46 +0100

    Update four steps 60dc copy

--- src/pages/60-day-challenge.js
+++ src/pages/60-day-challenge.js
@@ -13,18 +13,20 @@ const Challenge = () => (
 		</div>
 		<p>That&rsquo;s right. Swap to walking and cycling with our 60 Day Challenge and you could win fantastic prizes, including Love2Shop vouchers, Amazon gift cards and even the chance to win a bike and accessories to the value of £1,000!</p>
 		<p>All you have to do to be in with a chance to win is take part in the 60 Day Challenge. It’s completely free and every trip counts. It doesn’t matter if you’re walking to work or simply cycling to the shops to pick up a pint of milk: tell us about your trip and you could win! It’s as simple as that.</p>
-		<h2>Four simple steps to starting your 60 Day Challenge:</h2>
+		<h2>Four simple steps to start your 60 Day Challenge</h2>
+    <p>Remember, you need to be over 18 and live in an eligible Essex area to take part. If you are younger or don’t live in these areas you can still join the community on <a href="https://www.facebook.com/stopswapgo/">Facebook</a>, <a href="https://twitter.com/stopswapgoessex">Twitter</a> and <a href="https://www.instagram.com/stopswapgo/">Instagram</a>. If you do, let’s get you started!</p>
 		<ol>
 			<li>
         <p>Complete the sign up form below. Remember, you need to be an Essex resident and over 18 to take part!</p>
-        <p>After you’ve submitted the form you’ll see a link to our challenge smartphone app called Indeemo.</p>
+        <p>After you’ve submitted the form registration for our challenge smartphone app, called Indeemo, will open.</p>
       </li>
 			<li>
-        <p>Click the link and create an Indeemo account using the same email address you used to register for the 60 Day Challenge.</p>
-        <p>You can either do this straight away using the link or check your inbox for an email from us. We’ll automatically send you an email with the link within 24 hours of you signing up. </p>
+        <p>Create an Indeemo account using the same email address you used to register for the 60 Day Challenge.</p>
+        <p>You can either do this straight away or check your inbox for an email from us. We’ll automatically send you an email with the link within 24 hours of you signing up. Don’t forget to check your junk mail folder if you can’t find it in your inbox.</p>
       </li>
       <li>
-        <p>Download the Indeemo app and log in to it via your smartphone.</p>
+        <p>Download the Indeemo app from a link on the page and log in to it via your smartphone.</p>
+        <p>You can either do this straight away or check your inbox from an email from Indeemo. They’ll automatically send you an email shortly after registering. Again, don’t forget to check your junk mail folder.</p>
       </li>
       <li>
         <p>Follow the quick app tutorial and start your challenge!</p>
