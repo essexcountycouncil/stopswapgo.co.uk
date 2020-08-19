ef4e3038c5f80793fec0151065d1cbde46369fd2
userman123 <dominic.freeman123@gmail.com>
2020-08-19 16:34:01 +0100

    Success message for 60dc

--- src/components/signup-sixty-day/signup-sixty-day.js
+++ src/components/signup-sixty-day/signup-sixty-day.js
@@ -41,165 +41,164 @@ export default function SignupSixtyDay() {
       <form onSubmit={handleSubmit(onSubmit)} className="newsletterForm form">
         <h3 className="sub-section-heading strapline strapline-highlighted"><span>STOP.SWAP.</span><i>SIGNUP!</i></h3>
         <p className="warning">Note * indicates required answer</p>
-        {submitted ?
-          (<div>
-            <SixtyDaySubmissionThankyou />
-          </div>) :
-          (<div>
-            <div>
-              <ul className="errors">
+        <div>
+          <ul className="errors">
 
-                {errors.agreement ? (<li className="error">{errors.agreement?.message}</li>) : (null)}
-              </ul>
+            {errors.agreement ? (<li className="error">{errors.agreement?.message}</li>) : (null)}
+          </ul>
+        </div>
+
+        <div className={"form-group group" + (errors.full_name?.message ? " error-block" : "")}>
+          <label className="section-heading">Your full name*</label>
+          {errors.full_name ? (<span className="error">{errors.full_name?.message}</span>) : (null)}
+          <input
+            name="full_name"
+            className={"large-input form-control" + (errors.full_name?.message ? " error" : "")}
+            type="text"
+            ref={register({
+              required: 'Enter your full name',
+              // pattern: {
+              //   value: EMAIL_REGEX,
+              //   message: 'Enter an email address in the correct format, like name@example.com',
+              // },
+            })}
+          />
+        </div>
+        <div className={"form-group group" + (errors.email?.message ? " error-block" : "")}>
+          <label className="section-heading">Your email*</label>
+          {errors.email ? (<span className="error">{errors.email?.message}</span>) : (null)}
+          <input
+            name="email"
+            className={"large-input form-control" + (errors.email?.message ? " error" : "")}
+            type="text"
+            ref={register({
+              required: 'Your email address is required',
+              pattern: {
+                value: EMAIL_REGEX,
+                message: 'Enter an email address in the correct format, like name@example.com',
+              },
+            })}
+          />
+        </div>
+        <div className={"form-group group" + (errors.postcode?.message ? " error-block" : "")}>
+          <label className="section-heading">Your postcode*</label>
+          {errors.postcode ? (<span className="error">{errors.postcode?.message}</span>) : (null)}
+          <input
+            name="postcode"
+            className={"large-input" + (errors.postcode?.message ? " error" : "")}
+            type="text"
+            ref={register({
+              required: 'Enter a real postcode',
+                // pattern: {
+                //   value: EMAIL_REGEX,
+                //   message: 'Enter an email address in the correct format, like name@example.com',
+                // },
+            })}
+          />
+        </div>
+        <div className={"form-group group" + (errors.over_eighteen?.message ? " error-block" : "")}>
+          <fieldset>
+            <legend>Are you 18 years of age or older?</legend>
+            <span className="form-hint"> Please note, only people aged 18 or over are able to take part in the 60 Day Challenge.</span>
+            {errors.over_eighteen ? (<span className="error">{errors.over_eighteen?.message}</span>) : (null)}
+            <div class="multiple-choice">
+              <input 
+                name="over_eighteen"
+                className={"large-input form-control" + (errors.over_eighteen?.message ? " error" : "")}
+                value="294706" 
+                type="checkbox" 
+                ref={register({ 
+                  required: 'Select yes only if you are 18 or older'
+                })}/>
+              <label>Yes</label>
             </div>
-
-            <div className={"form-group group" + (errors.full_name?.message ? " error-block" : "")}>
-              <label className="section-heading">Your full name*</label>
-              {errors.full_name ? (<span className="error">{errors.full_name?.message}</span>) : (null)}
+          </fieldset>
+        </div>
+        <div className={"form-group group" + (errors.council?.message ? " error-block" : "")}>
+          <fieldset>
+            <legend>What is the name of your local council?*</legend>
+            <span className="form-hint">If you don’t know, you can <a href="https://www.gov.uk/find-local-council">find your local council</a> on GOV.UK. Please note, only people in the eligible Essex areas listed are able to take part in the 60 DC.</span>
+            {errors.council ? (<span className="error">{errors.council?.message}</span>) : (null)}
+              <select 
+              name="council" 
+              className={"large-input" + (errors.council?.message ? " error" : "")}
+              ref={register({
+                required: 'Expand the list and select your local council',
+              })}>
+              <option value="" disabled selected>--Select your council--</option>
+              <option value="294330">Basildon</option>
+              <option value="294331">Braintree</option>
+              <option value="294332">Brentwood</option>
+              <option value="294333">Castle Point</option>
+              <option value="294334">Chelmsford</option>
+              <option value="294335">Colchester</option>
+              <option value="294336">Epping Forest</option>
+              <option value="294337">Harlow</option>
+              <option value="294338">Maldon</option>
+              <option value="294339">Rochford</option>
+              <option value="294340">Tendring</option>
+              <option value="294341">Uttlesford</option>
+            </select>
+          </fieldset>
+        </div>
+        <div className={"form-group group" + (errors.consent_to_share?.message ? " error-block" : "")}>
+          <fieldset>
+            <legend>Can we share your content?*</legend>
+            <p>As part of your 60 Day Challenge you will be asked to share your daily trips with short notes or photos and videos. If you would be happy for us to then share your responses as part of our marketing to help us get more people walking and cycling, please specify here. We’ll contact you if you’ve won a prize or to share the latest news about Stop.Swap.<i>GO!</i></p>
+            {errors.consent_to_share ? (<span className="error">{errors.consent_to_share?.message}</span>) : (null)}
+            <div class="multiple-choice">
               <input
-                name="full_name"
-                className={"large-input form-control" + (errors.full_name?.message ? " error" : "")}
-                type="text"
+                name="consent_to_share"
+                value="294328"
+                type="radio"
                 ref={register({
-                  required: 'Enter your full name',
-                  // pattern: {
-                  //   value: EMAIL_REGEX,
-                  //   message: 'Enter an email address in the correct format, like name@example.com',
-                  // },
+                  required: "Select yes if we can share your content or select no if not"
                 })}
               />
+              <label>Yes, you can share content I upload to the app</label>
             </div>
-            <div className={"form-group group" + (errors.email?.message ? " error-block" : "")}>
-              <label className="section-heading">Your email*</label>
-              {errors.email ? (<span className="error">{errors.email?.message}</span>) : (null)}
+            <div class="multiple-choice">
               <input
-                name="email"
-                className={"large-input form-control" + (errors.email?.message ? " error" : "")}
-                type="text"
+                name="consent_to_share"
+                value="294329"
+                type="radio"
                 ref={register({
-                  required: 'Your email address is required',
-                  pattern: {
-                    value: EMAIL_REGEX,
-                    message: 'Enter an email address in the correct format, like name@example.com',
-                  },
+                  required: "Select yes if we can share your content or select no if not"
                 })}
               />
+              <label>No, please do not share my content</label>
             </div>
-            <div className={"form-group group" + (errors.postcode?.message ? " error-block" : "")}>
-              <label className="section-heading">Your postcode*</label>
-              {errors.postcode ? (<span className="error">{errors.postcode?.message}</span>) : (null)}
-              <input
-                name="postcode"
-                className={"large-input" + (errors.postcode?.message ? " error" : "")}
-                type="text"
-                ref={register({
-                  required: 'Enter a real postcode',
-                    // pattern: {
-                    //   value: EMAIL_REGEX,
-                    //   message: 'Enter an email address in the correct format, like name@example.com',
-                    // },
-                })}
-              />
+          </fieldset>
+        </div>
+        <div className={"form-group group" + (errors.consent_data_privacy?.message ? " error-block" : "")}>
+          <fieldset>
+            {errors.consent_data_privacy ? (<span className="error">{errors.consent_data_privacy?.message}</span>) : (null)}
+            <div class="multiple-choice">
+              <input 
+                name="consent_data_privacy"
+                className={"large-input form-control" + (errors.consent_data_privacy?.message ? " error" : "")}
+                value="294706" 
+                type="checkbox" 
+                ref={register({ 
+                  required: 'Select the checkbox to consent to our data privacy policy'
+                })}/>
+              <label>By checking this box, you consent the data privacy polices of <a href="https://subscriberhelp.granicus.com/s/article/Privacy">GovDelivery</a> and <a href="https://www.essex.gov.uk/topic/privacy-and-data-protection">Essex County Council.</a></label>
             </div>
-            <div className={"form-group group" + (errors.over_eighteen?.message ? " error-block" : "")}>
-              <fieldset>
-                <legend>Are you 18 years of age or older?</legend>
-                <span className="form-hint"> Please note, only people aged 18 or over are able to take part in the 60 Day Challenge.</span>
-                {errors.over_eighteen ? (<span className="error">{errors.over_eighteen?.message}</span>) : (null)}
-                <div class="multiple-choice">
-                  <input 
-                    name="over_eighteen"
-                    className={"large-input form-control" + (errors.over_eighteen?.message ? " error" : "")}
-                    value="294706" 
-                    type="checkbox" 
-                    ref={register({ 
-                      required: 'Select yes only if you are 18 or older'
-                    })}/>
-                  <label>Yes</label>
-                </div>
-              </fieldset>
-            </div>
-            <div className={"form-group group" + (errors.council?.message ? " error-block" : "")}>
-              <fieldset>
-                <legend>What is the name of your local council?*</legend>
-                <span className="form-hint">If you don’t know, you can <a href="https://www.gov.uk/find-local-council">find your local council</a> on GOV.UK. Please note, only people in the eligible Essex areas listed are able to take part in the 60 DC.</span>
-                {errors.council ? (<span className="error">{errors.council?.message}</span>) : (null)}
-                  <select 
-                  name="council" 
-                  className={"large-input" + (errors.council?.message ? " error" : "")}
-                  ref={register({
-                    required: 'Expand the list and select your local council',
-                  })}>
-                  <option value="" disabled selected>--Select your council--</option>
-                  <option value="294330">Basildon</option>
-                  <option value="294331">Braintree</option>
-                  <option value="294332">Brentwood</option>
-                  <option value="294333">Castle Point</option>
-                  <option value="294334">Chelmsford</option>
-                  <option value="294335">Colchester</option>
-                  <option value="294336">Epping Forest</option>
-                  <option value="294337">Harlow</option>
-                  <option value="294338">Maldon</option>
-                  <option value="294339">Rochford</option>
-                  <option value="294340">Tendring</option>
-                  <option value="294341">Uttlesford</option>
-                </select>
-              </fieldset>
-            </div>
-            <div className={"form-group group" + (errors.consent_to_share?.message ? " error-block" : "")}>
-              <fieldset>
-                <legend>Can we share your content?*</legend>
-                <p>As part of your 60 Day Challenge you will be asked to share your daily trips with short notes or photos and videos. If you would be happy for us to then share your responses as part of our marketing to help us get more people walking and cycling, please specify here. We’ll contact you if you’ve won a prize or to share the latest news about Stop.Swap.<i>GO!</i></p>
-                {errors.consent_to_share ? (<span className="error">{errors.consent_to_share?.message}</span>) : (null)}
-                <div class="multiple-choice">
-                  <input
-                    name="consent_to_share"
-                    value="294328"
-                    type="radio"
-                    ref={register({
-                      required: "Select yes if we can share your content or select no if not"
-                    })}
-                  />
-                  <label>Yes, you can share content I upload to the app</label>
-                </div>
-                <div class="multiple-choice">
-                  <input
-                    name="consent_to_share"
-                    value="294329"
-                    type="radio"
-                    ref={register({
-                      required: "Select yes if we can share your content or select no if not"
-                    })}
-                  />
-                  <label>No, please do not share my content</label>
-                </div>
-              </fieldset>
-            </div>
-            <div className={"form-group group" + (errors.consent_data_privacy?.message ? " error-block" : "")}>
-              <fieldset>
-                {errors.consent_data_privacy ? (<span className="error">{errors.consent_data_privacy?.message}</span>) : (null)}
-                <div class="multiple-choice">
-                  <input 
-                    name="consent_data_privacy"
-                    className={"large-input form-control" + (errors.consent_data_privacy?.message ? " error" : "")}
-                    value="294706" 
-                    type="checkbox" 
-                    ref={register({ 
-                      required: 'Select the checkbox to consent to our data privacy policy'
-                    })}/>
-                  <label>By checking this box, you consent the data privacy polices of <a href="https://subscriberhelp.granicus.com/s/article/Privacy">GovDelivery</a> and <a href="https://www.essex.gov.uk/topic/privacy-and-data-protection">Essex County Council.</a></label>
-                </div>
-              </fieldset>
-            </div>       
+          </fieldset>
+        </div>       
 
-            <div className="button-container-middle">
-              <button type="submit" className="button button-dark button-large">SIGNUP!</button>
-            </div>
+        <div className="button-container-middle">
+          <button type="submit" className="button button-dark button-large">SIGNUP!</button>
+        </div>
 
-            <div className="disclaimer">
-              <p>By submitting your contact details, you acknowledge the <a href="https://www.stopswapgo.co.uk/terms-and-conditions">terms and conditions for the 60 Day Challenge</a>. Essex County Council is the controller of this personal information. If it looks like you might be committing fraud, we might have to share it with the police or fraud investigators. For more details about how we use your personal data, please read the <a href="https://subscriberhelp.granicus.com/s/article/Privacy">GovDelivery data privacy policy</a> and <a href="https://www.essex.gov.uk/topic/privacy-and-data-protection">Essex County Council’s data privacy policy.</a></p>
-            </div>
-          </div>)}
+        { (submitted && success)
+          ? <SixtyDaySubmissionThankyou />
+          : <p></p>
+        }
+
+        <div className="disclaimer">
+          <p>By submitting your contact details, you acknowledge the <a href="https://www.stopswapgo.co.uk/terms-and-conditions">terms and conditions for the 60 Day Challenge</a>. Essex County Council is the controller of this personal information. If it looks like you might be committing fraud, we might have to share it with the police or fraud investigators. For more details about how we use your personal data, please read the <a href="https://subscriberhelp.granicus.com/s/article/Privacy">GovDelivery data privacy policy</a> and <a href="https://www.essex.gov.uk/topic/privacy-and-data-protection">Essex County Council’s data privacy policy.</a></p>
+        </div>
       </form>
     </section>
   )
