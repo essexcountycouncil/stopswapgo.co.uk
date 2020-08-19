576dca817e1a3cd92309c8e1d246f14d50cf87e6
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-19 16:58:36 +0100

    add video

--- src/pages/getting-to-school/transport.js
+++ src/pages/getting-to-school/transport.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from 'gatsby'
+import Video from "../../components/video/video"
+import transortVideo from "../../video/2020.07.16_Face_CoveringsV2_MP4_40s.mp4"
 import Layout from "../../layout/layout-h2s-with-newsletter-no-banner"
 
 const Transport = () => (
@@ -9,7 +11,10 @@ const Transport = () => (
 	<p>We are asking all children who can to wear a <a href="https://www.gov.uk/government/publications/how-to-wear-and-make-a-cloth-face-covering">face covering</a>. Some children are&nbsp;<a href="https://www.gov.uk/guidance/coronavirus-covid-19-safer-travel-guidance-for-passengers#exemptions-face-coverings">exempt from wearing face coverings</a>.</p>
 	<p>Wearing a face covering helps keep everyone safe. It takes a bit of getting used to, but it&rsquo;s a simple measure most children can take. A face covering doesn&rsquo;t have to be a single use mask. It can be something you already have at home, like a scarf.&nbsp;<a href="https://www.gov.uk/government/publications/face-coverings-when-to-wear-one-and-how-to-make-your-own/face-coverings-when-to-wear-one-and-how-to-make-your-own#making-your-own-face-covering">Why not try making your own?</a></p>
 	<p>Please make sure your child has a face covering and knows how to use it. This video shows you how to wear a face covering.</p>
-	<p>&lt;insert video&gt;</p>
+	<Video 
+		videoSrcURL={transortVideo}
+		videoTitle="title"
+	/>
 	<p>Other changes you need to be aware of include:</p>
 	<ul>
 		<li>changes to how children get on the bus and where they can sit &ndash; social distancing is not required on dedicated school services so children might be asked to sit with their year group for example</li>
