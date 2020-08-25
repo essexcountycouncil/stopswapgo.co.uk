f4f3a7bf7cbe768b01b0ff39a90f94c436f5c3da
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-25 15:36:08 +0100

    add to getting help

--- src/components/social/social-g2s.js
+++ src/components/social/social-g2s.js
@@ -1,18 +1,27 @@
 import React from "react"
 export default () =>
 	<div>
-		<div className="previews social-feed previews-grey-border">
+		<div className="social">
+			<h2 className="strapline">Stop.Swap.<i>SHARE!</i></h2>
+			<p>Join our community on social media and share your active travel school stories, tips and tricks. We’ll also be posting updates on getting to school information – including details of a competition where prizes are on offer! Keep your eyes peeled, everyone.</p>
+			<p>#StopSwapGo #StopSwapWin</p>
+			<ul className="social-icons">
+				<li>
+					<a href="https://www.twitter.com/stopswapgoessex" className="twitter"><span className="fab fa-twitter" title="Twitter" aria-hidden="true"></span><span>Twitter</span></a></li><li>
+					<a href="https://www.facebook.com/stopswapgo" className="facebook"><span className="fab fa-facebook-f" title="Facebook" aria-hidden="true"></span><span>Facebook</span></a></li><li>
+					<a href="https://instagram.com/stopswapgo" className="instagram"><span className="fab fa-instagram" title="Instagram" aria-hidden="true"></span><span>Instagram</span></a></li>
+			</ul>
+			<div className="preview">		
+				<h3>Tweet with us on Twitter</h3>		
+				<a className="twitter-timeline"  data-width="500" data-height="400" href="https://twitter.com/stopswapgoessex?ref_src=twsrc%5Etfw">Tweets by stopswapgoessex</a>
+			</div>
 			<div className="preview">
-				<h2 className="strapline">Stop.Swap.<i>SHARE!</i></h2>
-				<p>Join our community on social media and share your active travel school stories, tips and tricks. We’ll also be posting updates on getting to school information – including details of a competition where prizes are on offer! Keep your eyes peeled, everyone.</p>
-				<p>#StopSwapGo #StopSwapWin</p>
-				<ul className="social">
-					<li>
-						<a href="https://www.twitter.com/stopswapgoessex" className="twitter"><span className="fab fa-twitter" title="Twitter" aria-hidden="true"></span><span>Twitter</span></a></li><li>
-						<a href="https://www.facebook.com/stopswapgo" className="facebook"><span className="fab fa-facebook-f" title="Facebook" aria-hidden="true"></span><span>Facebook</span></a></li><li>
-						<a href="https://instagram.com/stopswapgo" className="instagram"><span className="fab fa-instagram" title="Instagram" aria-hidden="true"></span><span>Instagram</span></a></li>
-				</ul>			
-				<a className="twitter-timeline" data-height="960" href="https://twitter.com/stopswapgoessex?ref_src=twsrc%5Etfw">Tweets by stopswapgoessex</a>
+				<h3>Follow us on Facebook</h3>
+				<div class="fb-page" data-href="https://www.facebook.com/stopswapgo/" data-width="500" data-height="400"  data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+					<blockquote cite="https://www.facebook.com/stopswapgo/" class="fb-xfbml-parse-ignore">
+						<a href="https://www.facebook.com/stopswapgo/">Stop Swap Go</a>
+					</blockquote>
+				</div>
 			</div>
 		</div>
 	</div>
