6279f8ad521d1e6c600a092096ce9ae5ee81b72d
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-26 14:25:19 +0100

    remove iframe and use vide tag only

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,16 +1,7 @@
 import React from "react"
-const Video = ({ videoSrcURL, videoTitle, ...props }) => (
-  <div className="video">
-    <iframe
-      src={videoSrcURL}
-      title={videoTitle}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      autoPlay={false}
-      frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
-      allowFullScreen
-    />
-  </div>
+const Video = ({ videoSrcURL, ...props }) => (
+  <video className="video" controls>
+    <source src={videoSrcURL} type="video/mp4" />
+  </video>
 )
 export default Video
