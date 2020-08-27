e1f2d16f387ac894abf99ec3eb56865e81883d51
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-08-27 11:47:06 +0100

    temp index api setuo

---
+++ src/pages/_index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Helmet from "react-helmet"
+import LayoutWithNewsletter from "../layout/layout-with-newsletter"
+import PreviewServices from "../components/preview/preview-services"
+import SocialFeed from "../components/social/social"
+
+export default ({ data }) => {
+  return (
+    <LayoutWithNewsletter>
+    <Helmet>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous" />
+    </Helmet>
+    <div className="mantra" id="get-ready">
+    	<h2 className="strapline">Get ready to Stop.Swap.<i>GO!</i></h2>
+      <br/>
+    	<h3>Hello! We’re making getting around Essex easier – and more rewarding – than ever.</h3>
+      <p>More and more of us are swapping from car to bus, bike or our own two feet. And whether you’re off to the shops, visiting friends or family, or getting to and from work, school or college, we’re here to help keep you moving.</p>
+      <p>Join our community for the latest events, news and journey info. We’ll also be giving away prizes – including the chance to win a bike and cycling accessories worth £1,000 – with our 60 Day Challenge.</p>
+    </div>
+  	<PreviewServices />
+    <SocialFeed />
+    </LayoutWithNewsletter>
+  )
+}
