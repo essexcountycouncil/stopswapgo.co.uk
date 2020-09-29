9d625b980363541385953384292e46668e1256ad
Martin J Thompson <martinjthompson@lucias-mbp.lan>
2020-09-29 16:52:53 +0100

    add embed green box

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BLOCKS } from "@contentful/rich-text-types"
 import { graphql } from "gatsby"
 import * as PropTypes from "prop-types"
 import Link from 'gatsby-link'
@@ -9,14 +10,25 @@ const propTypes = {
     data: PropTypes.object.isRequired,
 }
 
+const CalloutFull = ({ children }) => <div className="callout">{children}</div>
+const CalloutSide = ({ children }) => <div className="callout callout-side">{children}</div>
+
+
+
 class Page extends React.Component {
   render() {
     const page = this.props.data.contentfulPage
+    const callout = this.props.data.contentfulCallout
+    const options = {
+      renderNode: {
+         [BLOCKS.EMBEDDED_ENTRY]: (node, children) => <CalloutSide><p className="section-heading">{callout.title}</p>{documentToReactComponents(callout.content.json, options)}</CalloutSide>,
+      },  
+    }    
     return (
       <div>
         <Layout>
           <h1>{page.title}</h1>
-          {documentToReactComponents(page.content.json)}
+          {documentToReactComponents(page.content.json, options)}
         </Layout>
       </div>
     )
@@ -26,13 +38,19 @@ class Page extends React.Component {
 export default Page
 
 export const pageQuery = graphql`
-    query pageQuery( $id : String! ) {
-        contentfulPage( id: { eq: $id } ) {
-            id
-            title
-            content {
-                json
-            }
-        }
+  query pageQuery( $id : String! ) {
+    contentfulPage( id: { eq: $id } ) {
+      id
+      title
+      content {
+        json
+      }
     }
+    contentfulCallout {
+      title
+      content {
+        json
+      }
+    }    
+  }
 `
